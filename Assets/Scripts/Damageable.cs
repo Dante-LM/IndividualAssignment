3571678085f33bb9e088e3f9b0d91c6a6cb3bd4d
Dante-LM <dante.lomonaco@gmail.com>
2021-10-26 11:22:03 -0400

    Added basic targets
    Added some basic cube targets to test target functionality.

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Damageable : MonoBehaviour
 {
     [SerializeField] GameObject hitEffect;
+    [SerializeField] int health;
     void Start()
     {
         
     }
 
     public void TakeDamage(Vector3 hitPos, Vector3 hitNormal)
-    {
+    {        
         Instantiate(hitEffect, hitPos, Quaternion.LookRotation(hitNormal));
+        health--;        
     }
 
     void Update()
     {
-        
+        if (health == 0)
+            Destroy(this.gameObject);
     }
 }
